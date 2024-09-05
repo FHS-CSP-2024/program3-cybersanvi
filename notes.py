e0242a701a72b6d5361e134ddcc82f034d41f2e7
Sanvi Mishra <sanvi14m@gmail.com>
2024-09-05 14:58:14 +0000

    I finished my notes

--- notes.py
+++ notes.py
@@ -45,3 +45,42 @@
 #print("\n") # newline
 #print("\t") # tab
 #print("\\") # \
+
+#Uppercase and Lowercase Matters in variable assignment
+myXVAL = 10
+myxVAL = 7
+print(myXVAL)
+
+name = "Chuck"
+number = 100
+newNumber = "100"
+
+print(number/2)
+#Casting Example
+print(int(newNumber)/2)
+
+myFloat = 2.234
+integer = 12
+
+num1 = 100
+num2 = 75
+num3 = 967
+
+avg = ((num1 + num2 + num3)/3)
+print("Average is {avg}")
+print(avg)
+print("Average ", avg)
+print("Average " + str(avg))
+
+bed= 5
+bath= 4
+address= "111 Cybersecurity Road"
+city = "Folsom"
+zip= 95630
+rent= 7000
+
+print(F"House for rent at {address} in {city} {zip}")
+print(F"\t{bed} bedroom, {bath} bathrooms")
+print(F"\tRent is ${rent}/month")
+
+print("I have a file located at: C:\\users\\Sanvi\\documents...")
